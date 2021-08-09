e0cc19c7fae10926db56a76455fbdbe4808d1e82
James <escrevaparajames@hotmail.com>
2021-08-09 11:16:57 -0300

    github

---
+++ Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using iText.Kernel.Pdf;
+using iText.Kernel.Pdf.Canvas.Parser;
+using iText.Kernel.Pdf.Canvas.Parser.Listener;
+
+
+namespace LeitorPdf
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            
+            Console.WriteLine(ExtrairTextodoPdf("TEST PDF TEXT.pdf"));
+            Console.ReadKey();
+        }
+        static string ExtrairTextodoPdf(string NomedoArquivo)
+        {
+            string result=null;
+            PdfReader pdfReader = new PdfReader(NomedoArquivo);
+            PdfDocument pdfDoc = new PdfDocument(pdfReader);
+            for (int page =1; page<=pdfDoc.GetNumberOfPages(); page++)
+            {
+                ITextExtractionStrategy strategy = new SimpleTextExtractionStrategy();
+                string conteudo = PdfTextExtractor.GetTextFromPage(pdfDoc.GetPage(page), strategy);
+                result += conteudo;
+            }
+            pdfDoc.Close();
+            pdfReader.Close();
+            return result;
+        }
+    }
+}
---
+++ README.md
@@ -0,0 +1,26 @@
+
+# Aplicação com C#
+
+> Um metódo para usar futuramente, que extrai dados de um pdf (formulários, tabelas)
+
+## Imagens
+
+<p>
+  <img src="img.png" width="500" title="preview">
+  <p>    </p>
+
+</p>
+
+## Resumo
+
+O objetivo era iniciar um meio para extrair texto de um pdf com um framework, pra futuramente torna-lo uma aplicação web ou aplicar em um sistema
+
+## Versões - Histórico
+
+* 0.0.1
+    * Criação da lógica
+
+## 
+
+[https://github.com/JamesOliveira1](https://github.com/JamesOliveira1/)
+
